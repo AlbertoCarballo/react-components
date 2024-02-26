d91e47c04ac9004c2d9c53882da1072971235fcd
Jesus Alberto Carballo Olachea <jesusalbec_21@alu.uabcs.mx>
2024-02-26 00:07:16 -0700

    new style for RecipeCards

--- src/componentes/RecipeCards.js
+++ src/componentes/RecipeCards.js
@@ -7,13 +7,13 @@ import './RecipeCardDesign.css'; // Asegúrate de tener la ruta correcta al arch
 function RecipeCards() {
     return (
         <Container>
-            <Row className=''>
+            <Row className='fila'>
                 {recipes.map((recipe, index) => (
                     // console.log(recipe, index)
-                    <Col key={index} lg={4}>
-                        <Card style={{ width: '100%', height: '100%' }}>
+                    <Col key={index} lg={3}>
+                        <Card style={{ width: '100%', height: '95%' }}>
                             <div className="card-image-wrapper">
-                                <Card.Img variant="top" src={recipe.imagen} className="img-product" />
+                                <Card.Img variant="top" src={recipe.imagen} className="img-product" style={{ width: '100%', height: '100%' }} />
                             </div>
                             <Card.Body>
                                 <Card.Title style={{ fontSize: "30px" }}>
@@ -39,34 +39,3 @@ function RecipeCards() {
 
 
 export default RecipeCards;
-
-// return (
-//     <Container>
-//         {chunkArray(recipes, 4).map((row, rowIndex) => (
-//             <Row key={rowIndex} className='fila'>
-//                 {row.map((recipe, colIndex) => (
-//                     <Col key={colIndex} md={3}>
-//                         <Card style={{ width: '100%', height: '100%' }}>
-//                             <div className="card-image-wrapper">
-//                                 <Card.Img variant="top" src={recipe.imagen} className="img-product" />
-//                             </div>
-//                             <Card.Body>
-//                                 <Card.Title style={{ fontSize: "30px" }}>
-//                                     <strong>
-//                                         {recipe.nombre}
-//                                     </strong>
-//                                 </Card.Title>
-//                                 <Card.Text style={{ fontSize: '20px' }}>
-//                                     {recipe.descripcion}
-//                                 </Card.Text>
-//                                 <div className="button-container">
-//                                     <Button variant="primary">Ver receta</Button>
-//                                 </div>
-//                             </Card.Body>
-//                         </Card>
-//                     </Col>
-//                 ))}
-//             </Row>
-//         ))}
-//     </Container>
-// );
